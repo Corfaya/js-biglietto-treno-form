bf9beae2f5be0f7a66fd35ab8e282fe9f0875568
Corfaya <lauracorrado0696@gmail.com>
2024-06-26 15:15:45 +0200

    [UPD] discount's amount variable created and added a conditional construction added for 2 different messages

--- js/script.js
+++ js/script.js
@@ -2,29 +2,34 @@
 const btn = document.getElementById("button");
 
 // addEventListener on btn
-btn.addEventListener("click", function() {
-    // input data recovery
-    let completeName = document.getElementById("completeName").value;
-    let km = document.getElementById("km").value;
-    let age = document.getElementById("age").value;
-    console.log(completeName, km, age)
-    // cost per km variable declaration
-    let costPerKm = 0.21;
-    // price variable declaration
-    let price = costPerKm * km;
-    // CONDITIONAL CONSTRUCTION
-    // reduction initialised to 0
-    let reduction = 0;
-    // if user is a minor...
-    if (age === "underage") {
-        reduction = 20 / 100;
-        console.log(reduction)
-    } // else if user is over 65
-    else if (age === "over_65") {
-        reduction = 40 / 100;
-        console.log(reduction)
-    }
-    // Ticket reduced price variable declaration
-    let reducedPrice = (price - ( price * reduction)).toFixed(2);
-    console.log(reducedPrice)
-})
+btn.addEventListener("click", function () {
+  // input data recovery
+  let completeName = document.getElementById("completeName").value;
+  let km = document.getElementById("km").value;
+  let age = document.getElementById("age").value;
+  // cost per km variable declaration
+  let costPerKm = 0.21;
+  // price variable declaration
+  let price = costPerKm * km;
+  // CONDITIONAL CONSTRUCTION
+  // reduction initialised to 0
+  let reduction = 0;
+  // if user is a minor...
+  if (age === "underage") {
+    reduction = 20 / 100;
+  } // else if user is over 65
+  else if (age === "over_65") {
+    reduction = 40 / 100;
+  }
+  // Discount's amount variable
+  let discount = (price * reduction).toFixed(2);
+
+  // Ticket reduced price variable declaration
+  let reducedPrice = (price - discount).toFixed(2);
+  
+  if (age === "underage" || age === "over_65") {
+    console.log("L'utente " + completeName + " ha diritto a uno sconto di " + discount + "€. Il suo prezzo finale è pari a " + reducedPrice + "€.");
+  } else {
+    console.log("L'utente " + completeName + " non ha diritto a uno sconto. Il suo prezzo finale è pari a " + reducedPrice + "€.");
+  }
+});
