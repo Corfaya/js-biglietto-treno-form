d32c8bed372c180fab0a0e93c1866d136d5c35f9
Corfaya <lauracorrado0696@gmail.com>
2024-06-26 14:40:00 +0200

    [UPD] addEventListener on button with input data recovered and showed on console

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // HTML button recovery
 const btn = document.getElementById("button");
+
+// addEventListener on btn
+btn.addEventListener("click", function() {
+    // input data recovery
+    let completeName = document.getElementById("completeName").value;
+    let km = document.getElementById("km").value;
+    console.log(completeName, km)
+})
